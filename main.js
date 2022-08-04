311aa1cb9873906b3226e359474d5e333f0328e6
Abhishek <abhishek@pop-os.localdomain>
2022-08-04 12:19:40 +0530

    completed design now full

--- main.js
+++ main.js
@@ -1,49 +1,96 @@
 // Constants and Variables
-const menuBtn = document.querySelector('.menu--btn');
-const menuImg = document.querySelector('.menu--btn img');
-const navbar = document.querySelector('.nav--list');
-const bookmark = document.querySelector('.bookmark');
-const bookmarkText = document.querySelector('.bookmark span');
-const pledgeCard = document.querySelectorAll('.pledge--card');
-const rewardBtn = document.querySelectorAll('.reward--btn');
-
-
-
+const menuBtn = document.querySelector(".menu--btn");
+const menuImg = document.querySelector(".menu--btn img");
+const navbar = document.querySelector(".nav--list");
+const bookmark = document.querySelector(".bookmark");
+const bookmarkText = document.querySelector(".bookmark span");
+const pledgeCard = document.querySelectorAll(".pledge--card");
+const rewardBtn = document.querySelectorAll(".reward--btn");
+const supportWrapper = document.querySelector(".support--wrapper");
+const projectBtn = document.querySelector(".btn");
+const gotBtn = document.querySelector('.support--container button');
+const pledgeContainer = document.querySelector('.pledge--choose--wrapper');
+const hideNavbar = document.querySelector('.navbar');
+const pledgeCards = document.querySelectorAll('.pledge--choose--card');
+const circleBtns = document.querySelectorAll('.one');
+const closeBtn = document.querySelector('.pledge--choose--container img');
+let isClick = false;
 
 // Functions
 
 const handleClick = () => {
-   navbar.classList.toggle('active');
-   if(navbar.classList.contains('active')) {
+  navbar.classList.toggle("active");
+  if (navbar.classList.contains("active")) {
     menuImg.src = "./images/icon-close-menu.svg";
-   }
-   else {
+  } else {
     menuImg.src = "./images/icon-hamburger.svg";
-   }
-}
+  }
+};
 
 const handleBookmark = () => {
-   bookmark.classList.toggle('bookmarked');
-   if(bookmark.classList.contains('bookmarked')) {
-      bookmarkText.textContent = "bookmarked";
-   }
-   else {
-      bookmarkText.textContent = "bookmark";
-   }
-}
+  bookmark.classList.toggle("bookmarked");
+  if (bookmark.classList.contains("bookmarked")) {
+    bookmarkText.textContent = "bookmarked";
+  } else {
+    bookmarkText.textContent = "bookmark";
+  }
+};
 
 const handlePledgeButton = () => {
-  for(var i = 0; i < 3; i++) {
-   var currentCard = document.getElementsByClassName('fade')
-   for(var j = 0; j < currentCard.length; j++) {
-     let index = currentCard[j].dataset.index;
-     rewardBtn[index].textContent = 'Out of Stock';
-   }
-  } 
-}
+  for (var i = 0; i < 3; i++) {
+    var currentCard = document.getElementsByClassName("fade");
+    for (var j = 0; j < currentCard.length; j++) {
+      let index = currentCard[j].dataset.index;
+      rewardBtn[index].textContent = "Out of Stock";
+    }
+  }
+};
 handlePledgeButton();
 
-// EventListeners 
+const handleSupport = () => {
+  if (!supportWrapper.classList.contains("show")) {
+    supportWrapper.classList.add("show");
+    document.body.classList.add("active");
+    isClick = true;
+  } else {
+    supportWrapper.classList.remove("show");
+    document.body.classList.remove("active");
+    isClick = false;
+  }
+};
 
-menuBtn.addEventListener('click', handleClick)
-bookmark.addEventListener('click', handleBookmark)
+const gotHandle = () => {
+   if(!isClick) return; // prevent the function from running
+   supportWrapper.classList.remove('show');
+   pledgeContainer.classList.add('started');
+   if(pledgeContainer.classList.contains('started')) {
+      hideNavbar.style.zIndex = "1";
+   }
+   else {
+      hideNavbar.style.zIndex = "30"
+   }
+}
+
+function handleSelect(e) {
+   circleBtns.forEach(btn => btn.classList.remove('selected'));
+   pledgeCards.forEach(card => card.classList.remove('select'));
+   let index = this.dataset.select;
+   circleBtns[index].classList.add('selected');
+   if(pledgeCards[index].classList.contains('fade--out') || pledgeCards[index].classList.contains('notActive')) return; 
+   pledgeCards[index].classList.add('select');
+}
+
+const closePledge = () => {
+   pledgeContainer.classList.remove('started');
+   isClick = false;
+   hideNavbar.style.zIndex = "30";
+}
+
+// EventListeners
+
+menuBtn.addEventListener("click", handleClick);
+bookmark.addEventListener("click", handleBookmark);
+projectBtn.addEventListener("click", handleSupport);
+gotBtn.addEventListener('click', gotHandle)
+circleBtns.forEach(btn => btn.addEventListener('click', handleSelect));
+closeBtn.addEventListener('click', closePledge);
